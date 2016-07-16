9de27e88d32c5ed6512e38ad3f3d3b7e27b5bc29
mfuhrmeister <marcus.fuhrmeister@googlemail.com>
2016-07-16 15:49:41 +0200

    feat (nems) #397 implement error object

--- test/unit/error/errorsSpec.js
+++ test/unit/error/errorsSpec.js
@@ -7,75 +7,69 @@ describe('errors', function () {
     expect(errors).toBeDefined();
   });
 
-  describe('DownloadError', function () {
+  function testError(expectedErrorObject) {
+    describe(expectedErrorObject.name, function () {
 
-    var error;
+      var error;
 
-    beforeEach(function() {
-      error = new errors.DownloadError();
-    });
-
-    describe('properties', function() {
-
-      it('should be defined appropriately', function() {
-        expect(error.name).toBe('DownloadError');
-        expect(error.message).toBe('Download went wrong!');
-        expect(error.statusCode).toBe(404);
-        expect(error.predicate).toEqual(jasmine.any(Function));
+      beforeEach(function() {
+        error = new errors[expectedErrorObject.name]();
       });
 
-      it('predicate should return true, when called with same Error type', function () {
-        var expectedError = new errors.DownloadError();
-        expect(error.predicate(expectedError)).toBeTruthy();
+      describe('properties', function() {
+
+        it('should be defined appropriately', function() {
+          expect(error.name).toBe(expectedErrorObject.name);
+          expect(error.message).toBe(expectedErrorObject.message);
+          expect(error.statusCode).toBe(expectedErrorObject.statusCode);
+          expect(error.predicate).toEqual(jasmine.any(Function));
+        });
+
+        it('predicate should return false, when called with another Error type', function () {
+          var expectedError = new errors[expectedErrorObject.name]();
+          expectedError.name = 'ANY_OTHER_ERROR';
+          expect(error.predicate(expectedError)).toBeFalsy();
+        });
+
+        it('predicate should return true, when called with same Error type', function () {
+          var expectedError = new errors[expectedErrorObject.name]();
+          expect(error.predicate(expectedError)).toBeTruthy();
+        });
+
       });
 
-    });
+      it('should be initialized with custom message and status code', function () {
+        var
+          expectedStatusCode = 999,
+          expectedMessage = 'any additional error message';
 
-    it('should be initialized with custom message and status code', function () {
-      var
-        expectedStatusCode = 500,
-        expectedMessage = 'any additional error message';
+        error = new errors[expectedErrorObject.name](expectedMessage, expectedStatusCode);
 
-      error = new errors.DownloadError(expectedMessage, expectedStatusCode);
-
-      expect(error.statusCode).toBe(expectedStatusCode);
-      expect(error.message).toBe(expectedMessage);
-    });
-  });
-
-  describe('WriteError', function () {
-
-    var error;
-
-    beforeEach(function() {
-      error = new errors.WriteError();
-    });
-
-    describe('properties', function() {
-
-      it('should be defined appropriately', function() {
-        expect(error.name).toBe('WriteError');
-        expect(error.message).toBe('Writing file went wrong!');
-        expect(error.statusCode).toBe(500);
-        expect(error.predicate).toEqual(jasmine.any(Function));
+        expect(error.statusCode).toBe(expectedStatusCode);
+        expect(error.message).toBe(expectedMessage);
       });
-
-      it('predicate should return true, when called with same Error type', function () {
-        var expectedError = new errors.WriteError();
-        expect(error.predicate(expectedError)).toBeTruthy();
-      });
-
     });
+  }
 
-    it('should be initialized with custom message and status code', function () {
-      var
-        expectedStatusCode = 500,
-        expectedMessage = 'any additional error message';
+  var expectedErrors = [
+    {
+      name : 'DownloadError',
+      message: 'Download went wrong!',
+      statusCode: 404
+    },
+    {
+      name : 'WriteError',
+      message: 'Writing file went wrong!',
+      statusCode: 500
+    },
+    {
+      name : 'ExtractionError',
+      message: 'Extracting file went wrong!',
+      statusCode: 500
+    }
+  ];
 
-      error = new errors.WriteError(expectedMessage, expectedStatusCode);
-
-      expect(error.statusCode).toBe(expectedStatusCode);
-      expect(error.message).toBe(expectedMessage);
-    });
+  expectedErrors.forEach(function(errorObject) {
+    testError(errorObject);
   });
 });
