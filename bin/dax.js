2e51c5b192d527a69cb2795cc811596baf4ac5b9
mfuhrmeister <marcus.fuhrmeister@googlemail.com>
2016-07-26 15:25:07 +0200

    feat (nems) enrich message output in binary script

--- bin/dax.js
+++ bin/dax.js
@@ -2,6 +2,7 @@
 
 var
   logger = require('npmlog'),
+  sprintf = require('sprintf-js').sprintf,
   args = process.argv.splice(process.execArgv.length + 2),
   
   nems = require('../lib/nems'),
@@ -9,7 +10,10 @@ var
   MODULE_NAME = require('../package.json').name,
   MESSAGE_START = 'Starting distribution of mongodb!',
   MESSAGE_USAGE = 'Usage:\n\nnode ./lib/nems.js [version [dir]]\n\nversion - the version of the mongodb\ndir - the directory to download and extract to',
-  MESSAGE_DEFAULTS= 'Using default configuration for download and extraction.';
+  MESSAGE_DEFAULTS = 'Using default configuration for download and extraction.',
+  MESSAGE_FAILURE = 'Download or extraction failed. No file information returned from extraction',
+  MESSAGE_EXTRACTED = 'Extracted %s files to %s.',
+  MESSAGE_SUCCESS = 'Download and extraction completed.';
 
 logger.info(MODULE_NAME, MESSAGE_START);
 
@@ -24,4 +28,12 @@ var
   VERSION = (args[0]) ? args[0] : '2.4.9',
   DOWNLOAD_DIR = args[1];
 
-nems.distribute(VERSION, DOWNLOAD_DIR);
+nems.distribute(VERSION, DOWNLOAD_DIR)
+  .then(function(files) {
+    if (!files || files.length === 0) {
+      logger.info(MODULE_NAME, MESSAGE_FAILURE);
+      return;
+    }
+    logger.info(MODULE_NAME, sprintf(MESSAGE_EXTRACTED, files.length, files[0]['base']));
+    logger.info(MODULE_NAME, MESSAGE_SUCCESS);
+  });
