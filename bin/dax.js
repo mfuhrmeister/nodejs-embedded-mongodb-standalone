5114f9865e460bfe2e93fc48b72d465a25a6a5f3
mfuhrmeister <marcus.fuhrmeister@googlemail.com>
2016-07-27 16:13:04 +0200

    feat (nems) enrich usage message output in binary script

--- bin/dax.js
+++ bin/dax.js
@@ -9,20 +9,25 @@ var
 
   MODULE_NAME = require('../package.json').name,
   MESSAGE_START = 'Starting distribution of mongodb!',
-  MESSAGE_USAGE = 'Usage:\n\nnode ./lib/nems.js [version [dir]]\n\nversion - the version of the mongodb\ndir - the directory to download and extract to',
+  MESSAGE_USAGE = 'Usage:\n\nnode ./bin/dax.js [version [dir]]\n\n' +
+    'version - the version of the mongodb\n' +
+    'dir - the directory to download and extract to\n\n' +
+    'If no download directory is given, mongodb will be downloaded and extracted to the temporary folder of your OS.\n' +
+    'If no version is given also, a default version will be downloaded and extracted.',
   MESSAGE_DEFAULTS = 'Using default configuration for download and extraction.',
   MESSAGE_FAILURE = 'Download or extraction failed. No file information returned from extraction',
   MESSAGE_EXTRACTED = 'Extracted %s files to %s.',
   MESSAGE_SUCCESS = 'Download and extraction completed.';
 
-logger.info(MODULE_NAME, MESSAGE_START);
 
-if (args.length > 2) {
+if (args.length > 2 || (args.length === 1 && ( args[0] === 'h' || args[0] === '-h' || args[0] === '--help'))) {
   logger.warn(MODULE_NAME, MESSAGE_USAGE);
   return;
 } else if (args.length === 0) {
   logger.info(MODULE_NAME, MESSAGE_DEFAULTS);
 }
+
+logger.info(MODULE_NAME, MESSAGE_START);
 
 var
   VERSION = (args[0]) ? args[0] : '2.4.9',
