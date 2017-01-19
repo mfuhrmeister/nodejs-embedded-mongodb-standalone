0e280a63aeab3eecffc4d0202f81c2843d813eae
mfuhrmeister <marcus.fuhrmeister@googlemail.com>
2017-01-19 18:24:11 +0100

    feat (nems) #417 add download to interface

--- test/unit/nemsSpec.js
+++ test/unit/nemsSpec.js
@@ -13,7 +13,9 @@ describe('index', function() {
 
     ANY_VERSION = 'ANY_VERSION',
     ANY_DOWNLOAD_DIR = 'ANY_DOWNLOAD_DIR',
-    ANY_FILE;
+    ANY_DB_PATH = 'ANY_DB_PATH',
+    ANY_PORT = 'ANY_PORT',
+    ANY_FILE = 'ANY_FILE';
 
   beforeEach(function () {
     underTest = rewire('../../lib/nems.js');
@@ -24,8 +26,6 @@ describe('index', function() {
     extractionServiceMock = jasmine.createSpyObj('extractionService', ['extract']);
     extractionServiceMock.extract.and.returnValue(promise.resolve(ANY_FILE));
 
-
-
     underTest.__set__('downloadService', downloadServiceMock);
     underTest.__set__('extractionService', extractionServiceMock);
   });
@@ -34,45 +34,31 @@ describe('index', function() {
     expect(underTest).toBeDefined();
   });
 
-  describe('distribute', function() {
+  function testDownload(value) {
+    describe(value, function() {
+      it('should reject a promise if download fails', function (done) {
+        var expectedError = new Error('any download error');
+        downloadServiceMock.download.and.returnValue(promise.reject(expectedError));
 
-    it('should reject a promise if download fails', function (done) {
-      var expectedError = new Error('any download error');
-      downloadServiceMock.download.and.returnValue(promise.reject(expectedError));
+        underTest[value]().then(function () {
+          done.fail('Error should have been caught');
+        }).catch(function (err) {
+          expect(err).toEqual(expectedError);
+          done();
+        });
+      });
 
-      underTest.distribute().then(function () {
-        done.fail('Error should have been caught');
-      }).catch(function (err) {
-        expect(err).toEqual(expectedError);
-        done();
+      it('should call download on downloadService', function (done) {
+        underTest[value](ANY_VERSION, ANY_DOWNLOAD_DIR).then(function () {
+          expect(downloadServiceMock.download).toHaveBeenCalledWith(ANY_VERSION, ANY_DOWNLOAD_DIR);
+          done();
+        });
       });
     });
+  }
 
-    it('should reject a promise if extraction fails', function (done) {
-      var expectedError = new Error('any extraction error');
-      extractionServiceMock.extract.and.returnValue(promise.reject(expectedError));
-
-      underTest.distribute().then(function () {
-        done.fail('Error should have been caught');
-      }).catch(function (err) {
-        expect(err).toEqual(expectedError);
-        done();
-      });
-    });
-
-    it('should call download', function (done) {
-      underTest.distribute(ANY_VERSION, ANY_DOWNLOAD_DIR).then(function () {
-        expect(downloadServiceMock.download).toHaveBeenCalledWith(ANY_VERSION, ANY_DOWNLOAD_DIR);
-        done();
-      });
-    });
-
-    it('should call extract', function (done) {
-      underTest.distribute(ANY_VERSION, ANY_DOWNLOAD_DIR).then(function (files) {
-        expect(extractionServiceMock.extract).toHaveBeenCalledWith(ANY_FILE, ANY_VERSION, ANY_DOWNLOAD_DIR);
-        expect(files).toEqual(ANY_FILE);
-        done();
-      });
-    });
+  ['distribute', 'download'].forEach(function(value) {
+    testDownload(value);
   });
+
 });
