a0b278972e587622b7c354707d37aedc53fd75fe
mfuhrmeister <marcus.fuhrmeister@googlemail.com>
2017-01-19 18:28:46 +0100

    feature (nems) #427 add startMongo and stopMongo to interface

--- test/unit/nemsSpec.js
+++ test/unit/nemsSpec.js
@@ -10,6 +10,7 @@ describe('index', function() {
     underTest,
     downloadServiceMock,
     extractionServiceMock,
+    mongoServiceMock,
 
     ANY_VERSION = 'ANY_VERSION',
     ANY_DOWNLOAD_DIR = 'ANY_DOWNLOAD_DIR',
@@ -26,8 +27,13 @@ describe('index', function() {
     extractionServiceMock = jasmine.createSpyObj('extractionService', ['extract']);
     extractionServiceMock.extract.and.returnValue(promise.resolve(ANY_FILE));
 
+    mongoServiceMock = jasmine.createSpyObj('mongoService', ['start', 'stop']);
+    mongoServiceMock.start.and.returnValue(promise.resolve(ANY_FILE));
+    mongoServiceMock.stop.and.returnValue(promise.resolve(ANY_FILE));
+
     underTest.__set__('downloadService', downloadServiceMock);
     underTest.__set__('extractionService', extractionServiceMock);
+    underTest.__set__('mongoService', mongoServiceMock);
   });
 
   it('should be defined', function () {
@@ -92,5 +98,33 @@ describe('index', function() {
   ['distribute', 'extract'].forEach(function(value) {
     testExtractionError(value);
   });
+
+  function testStartStopMongo(test) {
+    describe(test.method + 'Mongo', function() {
+      it('should reject a promise if ' + test.method + ' fails', function (done) {
+        var expectedError = new Error('any ' + test.method + ' error');
+        mongoServiceMock[test.method].and.returnValue(promise.reject(expectedError));
+
+        underTest[test.method+'Mongo']().then(function () {
+          done.fail('Error should have been caught');
+        }).catch(function (err) {
+          expect(err).toEqual(expectedError);
+          done();
+        });
+      });
+
+      it('should call ' + test.method, function(done) {
+        underTest[test.method+'Mongo'].apply(this, test.params).then(function () {
+          expect(mongoServiceMock[test.method].calls.argsFor(0)).toEqual(test.params);
+          done();
+        });
+      });
+    });
+  }
+
+  [
+    { method: 'start', params: [ANY_DB_PATH, ANY_PORT, true, true] },
+    { method: 'stop', params: [ANY_DB_PATH] }
+  ].forEach(testStartStopMongo);
 
 });
