06129fd6ac35f570a8e7671c07c1f0950e487f50
Marcus Fuhrmeister <marcus.fuhrmeister@googlemail.com>
2026-07-19 12:59:16 +0200

    refactor(extraction): replace decompress with extract-zip and tar
    - Replace deprecated `decompress` dependency with `extract-zip` and `tar` packages
    - Rewrite extraction service to handle zip, tar and tgz archives properly
    - Add automatic parent directory creation for extraction paths
    - Add security filter to block symbolic and hard links during extraction
    - Update unit tests to match new implementation
    - Update package dependencies and lockfile

--- test/unit/distributer/extractionServiceSpec.js
+++ test/unit/distributer/extractionServiceSpec.js
@@ -2,10 +2,13 @@
 
 var
   os = require('os'),
+  path = require('path'),
   rewire = require('rewire'),
   Promise = require('bluebird'),
 
   ANY_VALID_FILE = 'ANY_VALID_FILE.zip',
+  ANY_VALID_TAR_FILE = 'ANY_VALID_FILE.tar',
+  ANY_VALID_TGZ_FILE = 'ANY_VALID_FILE.tgz',
   ANY_VALID_FILE_PATH = 'ANY_VALID_FILE_PATH',
   ANY_VALID_VERSION = 'ANY_VALID_VERSION',
   ANY_EXTRACTION_BASE_DIR = 'ANY_EXTRACTION_BASE_DIR',
@@ -16,24 +19,29 @@ var
   ANY_ARCH_TYPE_ERROR_MESSAGE = 'ANY_ARCH_TYPE_ERROR_MESSAGE';
 
 function createDecompressMock() {
-  var decompressMock = jasmine.createSpy('Decompress').and.callFake(function (file, dest, options) {
-    return Promise.resolve([{ path: 'extracted_file' }]);
-  });
-  return decompressMock;
+  return jasmine.createSpy('extractZip').and.returnValue(Promise.resolve());
 }
 
 describe('extractionService', function () {
 
   var
     underTest,
-    decompressMock;
+    extractZipMock,
+    tarMock,
+    ensureDirMock;
 
   beforeEach(function () {
     underTest = rewire('../../../lib/distributer/extractionService.js');
 
-    decompressMock = createDecompressMock();
+    extractZipMock = createDecompressMock();
+    tarMock = {
+      x: jasmine.createSpy('tar.x').and.returnValue(Promise.resolve())
+    };
+    ensureDirMock = jasmine.createSpy('ensureDir').and.returnValue(Promise.resolve());
 
-    underTest.__set__('Decompress', decompressMock);
+    underTest.__set__('extractZip', extractZipMock);
+    underTest.__set__('tar', tarMock);
+    underTest.__set__('ensureDir', ensureDirMock);
     underTest.__set__('ExtractionError', Error);
     underTest.__set__('DEFAULT_EXTRACTION_DIR', ANY_EXTRACTION_DIR);
     underTest.__set__('ERROR_MESSAGE_NO_FILE_FOR_EXTRACTION', ANY_FILE_ERROR_MESSAGE);
@@ -74,24 +82,50 @@ describe('extractionService', function () {
   [null, undefined, ''].forEach(testForInvalidFile);
   [null, undefined, ''].forEach(testForInvalidVersion);
 
-  describe('decompress', function() {
+  describe('extract', function() {
 
-    it('should call Decompress with default extraction directory and options', function (done) {
+    it('should call extractZip with default extraction directory and options for zip files', function (done) {
       var
-        expectedExtractionPath = [os.tmpdir(), ANY_EXTRACTION_DIR, ANY_VALID_VERSION].join('/');
+        expectedExtractionPath = path.join(os.tmpdir(), ANY_EXTRACTION_DIR, ANY_VALID_VERSION);
 
       underTest.extract(ANY_VALID_FILE, ANY_VALID_VERSION).then(function () {
-        expect(decompressMock).toHaveBeenCalledWith(ANY_VALID_FILE, expectedExtractionPath, { strip: 1 });
+        expect(ensureDirMock).toHaveBeenCalledWith(path.resolve(expectedExtractionPath));
+        expect(extractZipMock).toHaveBeenCalledWith(ANY_VALID_FILE, { dir: path.resolve(expectedExtractionPath) });
         done();
       });
     });
 
-    it('should call Decompress with given extraction directory and options', function (done) {
+    it('should call tar.x with strip for tar files', function (done) {
       var
-        expectedExtractionPath = [ANY_EXTRACTION_BASE_DIR, ANY_EXTRACTION_DIR, ANY_VALID_VERSION].join('/');
+        expectedExtractionPath = path.join(ANY_EXTRACTION_BASE_DIR, ANY_EXTRACTION_DIR, ANY_VALID_VERSION);
 
-      underTest.extract(ANY_VALID_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
-        expect(decompressMock).toHaveBeenCalledWith(ANY_VALID_FILE, expectedExtractionPath, { strip: 1 });
+      underTest.extract(ANY_VALID_TAR_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
+        expect(ensureDirMock).toHaveBeenCalledWith(path.resolve(expectedExtractionPath));
+        expect(tarMock.x).toHaveBeenCalledWith(jasmine.objectContaining({
+          file: ANY_VALID_TAR_FILE,
+          cwd: path.resolve(expectedExtractionPath),
+          strip: 1,
+          gzip: false,
+          preservePaths: false,
+          filter: jasmine.any(Function)
+        }));
+        done();
+      });
+    });
+
+    it('should call tar.x with gzip for tgz files', function (done) {
+      var
+        expectedExtractionPath = path.join(ANY_EXTRACTION_BASE_DIR, ANY_EXTRACTION_DIR, ANY_VALID_VERSION);
+
+      underTest.extract(ANY_VALID_TGZ_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
+        expect(tarMock.x).toHaveBeenCalledWith(jasmine.objectContaining({
+          file: ANY_VALID_TGZ_FILE,
+          cwd: path.resolve(expectedExtractionPath),
+          strip: 1,
+          gzip: true,
+          preservePaths: false,
+          filter: jasmine.any(Function)
+        }));
         done();
       });
     });
@@ -109,9 +143,24 @@ describe('extractionService', function () {
       });
     });
 
-    it('should catch a decompress error', function (done) {
-      var expectedError = new Error('any decompress error');
-      decompressMock.and.returnValue(Promise.reject(expectedError));
+    it('should reject .tar.bz2 files', function (done) {
+      var
+        expectedError = new Error(ANY_ARCH_TYPE_ERROR_MESSAGE),
+        file = [ANY_VALID_FILE_PATH, 'tar.bz2'].join('.');
+
+      underTest.extract(file, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
+        done.fail('Error should have been caught');
+      }).catch(function (err) {
+        expect(err).toEqual(expectedError);
+        done();
+      });
+    });
+
+    it('should catch an extractZip error', function (done) {
+      var expectedError = new Error('any extraction error');
+      extractZipMock.and.callFake(function () {
+        return Promise.reject(expectedError);
+      });
 
       underTest.extract(ANY_VALID_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
         done.fail('Error should have been caught');
@@ -121,11 +170,23 @@ describe('extractionService', function () {
       });
     });
 
+    it('should catch a tar.x error', function (done) {
+      var expectedError = new Error('any extraction error');
+      tarMock.x.and.callFake(function () {
+        return Promise.reject(expectedError);
+      });
+
+      underTest.extract(ANY_VALID_TAR_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function () {
+        done.fail('Error should have been caught');
+      }).catch(function (err) {
+        expect(err).toEqual(expectedError);
+        done();
+      });
+    });
+
     it('should resolve with extraction directory', function (done) {
       var
-        expectedExtractionPath = [ANY_EXTRACTION_BASE_DIR, ANY_EXTRACTION_DIR, ANY_VALID_VERSION].join('/');
-
-      decompressMock.and.returnValue(Promise.resolve([{ path: 'extracted_file' }]));
+        expectedExtractionPath = path.join(ANY_EXTRACTION_BASE_DIR, ANY_EXTRACTION_DIR, ANY_VALID_VERSION);
 
       underTest.extract(ANY_VALID_FILE, ANY_VALID_VERSION, ANY_EXTRACTION_BASE_DIR).then(function (path) {
         expect(path).toEqual(expectedExtractionPath);
