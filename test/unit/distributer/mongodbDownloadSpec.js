e800300d3b4486e5201ef62bea273f11ce2ed9e7
Marcus Fuhrmeister <marcus.fuhrmeister@googlemail.com>
2026-07-21 12:02:45 +0200

    test: add mongodb download redirect and error coverage

---
+++ test/unit/distributer/mongodbDownloadSpec.js
@@ -0,0 +1,222 @@
+'use strict';
+
+var
+  events = require('events'),
+  path = require('path'),
+  rewire = require('rewire');
+
+function createFileStreamMock() {
+  var stream = new events.EventEmitter();
+
+  stream.close = jasmine.createSpy('close').and.callFake(function (callback) {
+    if (callback) {
+      callback();
+    }
+  });
+
+  return stream;
+}
+
+function createResponseMock(statusCode, headers) {
+  var response = new events.EventEmitter();
+
+  response.statusCode = statusCode;
+  response.headers = headers || {};
+  response.pipe = jasmine.createSpy('pipe');
+
+  return response;
+}
+
+function flushPromises() {
+  return new Promise(function (resolve) {
+    setImmediate(resolve);
+  });
+}
+
+function createEnoentError() {
+  var err = new Error('not found');
+  err.code = 'ENOENT';
+  return err;
+}
+
+describe('mongodbDownload', function () {
+
+  var
+    underTest,
+    downloadToFile,
+    fsMock,
+    httpsMock,
+    getosMock;
+
+  beforeEach(function () {
+    fsMock = {
+      promises: {
+        stat: jasmine.createSpy('fs.promises.stat').and.callFake(function () {
+          return Promise.reject(createEnoentError());
+        }),
+        mkdir: jasmine.createSpy('fs.promises.mkdir').and.returnValue(Promise.resolve()),
+        unlink: jasmine.createSpy('fs.promises.unlink').and.returnValue(Promise.resolve()),
+        rename: jasmine.createSpy('fs.promises.rename').and.returnValue(Promise.resolve())
+      },
+      createWriteStream: jasmine.createSpy('fs.createWriteStream')
+    };
+
+    httpsMock = {
+      get: jasmine.createSpy('https.get')
+    };
+
+    getosMock = jasmine.createSpy('getos');
+
+    underTest = rewire('../../../lib/distributer/mongodbDownload.js');
+    underTest.__set__('fs', fsMock);
+    underTest.__set__('https', httpsMock);
+    underTest.__set__('getos', getosMock);
+
+    downloadToFile = underTest.__get__('downloadToFile');
+  });
+
+  it('should be defined', function () {
+    expect(underTest).toBeDefined();
+  });
+
+  describe('downloadToFile', function () {
+
+    it('should follow redirects and resolve relative redirect locations', async function () {
+      var firstFile = createFileStreamMock();
+      var secondFile = createFileStreamMock();
+      var firstRequest = new events.EventEmitter();
+      var secondRequest = new events.EventEmitter();
+      var firstResponse = createResponseMock(302, { location: '/redirected/file.tgz' });
+      var secondResponse = createResponseMock(200);
+      var promise;
+
+      fsMock.createWriteStream.and.returnValues(firstFile, secondFile);
+      httpsMock.get
+        .withArgs(jasmine.objectContaining({
+          protocol: 'https:',
+          hostname: 'fastdl.mongodb.org',
+          path: '/linux/original.tgz'
+        }), jasmine.any(Function))
+        .and.callFake(function (_options, callback) {
+          callback(firstResponse);
+          return firstRequest;
+        });
+      httpsMock.get
+        .withArgs(jasmine.objectContaining({
+          protocol: 'https:',
+          hostname: 'fastdl.mongodb.org',
+          path: '/redirected/file.tgz'
+        }), jasmine.any(Function))
+        .and.callFake(function (_options, callback) {
+          callback(secondResponse);
+          return secondRequest;
+        });
+
+      promise = downloadToFile(
+        'https://fastdl.mongodb.org/linux/original.tgz',
+        '/tmp/file.tgz.in_progress',
+        '/tmp/file.tgz'
+      );
+
+      await flushPromises();
+      secondFile.emit('finish');
+
+      expect(await promise).toEqual('/tmp/file.tgz');
+      expect(fsMock.promises.unlink).toHaveBeenCalledWith('/tmp/file.tgz.in_progress');
+      expect(fsMock.promises.rename).toHaveBeenCalledWith('/tmp/file.tgz.in_progress', '/tmp/file.tgz');
+      expect(secondResponse.pipe).toHaveBeenCalledWith(secondFile);
+    });
+
+    it('should reject when the download status is not 200', async function () {
+      var request = new events.EventEmitter();
+      var response = createResponseMock(500);
+
+      fsMock.createWriteStream.and.returnValue(createFileStreamMock());
+      httpsMock.get.and.callFake(function (_options, callback) {
+        callback(response);
+        return request;
+      });
+
+      try {
+        await downloadToFile('https://fastdl.mongodb.org/linux/file.tgz', '/tmp/file.tgz.in_progress', '/tmp/file.tgz');
+        throw new Error('Expected downloadToFile to reject');
+      } catch (err) {
+        expect(err).toEqual(new Error('download failed with status 500'));
+      }
+    });
+
+    it('should reject when the request emits an error', async function () {
+      var request = new events.EventEmitter();
+      var expectedError = new Error('network failure');
+      var promise;
+
+      fsMock.createWriteStream.and.returnValue(createFileStreamMock());
+      httpsMock.get.and.returnValue(request);
+
+      promise = downloadToFile('https://fastdl.mongodb.org/linux/file.tgz', '/tmp/file.tgz.in_progress', '/tmp/file.tgz');
+      request.emit('error', expectedError);
+
+      try {
+        await promise;
+        throw new Error('Expected downloadToFile to reject');
+      } catch (err) {
+        expect(err).toBe(expectedError);
+      }
+    });
+
+    it('should reject when the response emits an error', async function () {
+      var request = new events.EventEmitter();
+      var response = createResponseMock(200);
+      var file = createFileStreamMock();
+      var expectedError = new Error('stream failure');
+      var promise;
+
+      fsMock.createWriteStream.and.returnValue(file);
+      httpsMock.get.and.callFake(function (_options, callback) {
+        callback(response);
+        return request;
+      });
+
+      promise = downloadToFile('https://fastdl.mongodb.org/linux/file.tgz', '/tmp/file.tgz.in_progress', '/tmp/file.tgz');
+      response.emit('error', expectedError);
+
+      try {
+        await promise;
+        throw new Error('Expected downloadToFile to reject');
+      } catch (err) {
+        expect(err).toBe(expectedError);
+      }
+    });
+  });
+
+  describe('mongodbDownload', function () {
+
+    it('should reject if version is missing', async function () {
+      try {
+        await underTest({
+          platform: 'win32',
+          arch: 'x64'
+        });
+        throw new Error('Expected mongodbDownload to reject');
+      } catch (err) {
+        expect(err).toEqual(new Error('missing version'));
+      }
+    });
+
+    it('should return the cached file when it already exists', async function () {
+      var expectedFile = path.resolve('/tmp/downloads', 'mongodb-download', 'mongodb-win32-x86_64-3.2.8.zip');
+
+      fsMock.promises.stat.and.returnValue(Promise.resolve({}));
+
+      expect(await underTest({
+        version: '3.2.8',
+        platform: 'win32',
+        arch: 'x64',
+        download_dir: '/tmp/downloads'
+      })).toEqual(expectedFile);
+
+      expect(fsMock.promises.mkdir).toHaveBeenCalledWith(path.resolve('/tmp/downloads', 'mongodb-download'), { recursive: true });
+      expect(httpsMock.get).not.toHaveBeenCalled();
+    });
+  });
+});
